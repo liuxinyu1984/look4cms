3a30dd9dbb4ea61a744494e4b04e848572a8ea8b
liuxinyu1984 <liuxinyu1984@users.noreply.github.com>
2023-07-30 18:24:21 -0700

    create app: users

--- backend/urls.py
+++ backend/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pages/', include('pages.urls'))
+    path('pages/', include('pages.urls')),
+    path("users/", include("django.contrib.auth.urls")),
+    path('users/', include('user.urls')),
 ]
