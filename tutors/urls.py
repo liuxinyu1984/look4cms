1d1fed210ad9a1ec6976f8daf214c0109b7e3b8b
liuxinyu1984 <liuxinyu1984@users.noreply.github.com>
2023-08-11 17:34:41 -0700

    tutors: add view TutorCourseDetail, TutorLectureDetail

--- tutors/urls.py
+++ tutors/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import TutorCourseList
+from .views import TutorCourseList, TutorCourseDetail, TutorLectureDetail
 
 
 
 urlpatterns = [
     path('tutor_course_list', TutorCourseList.as_view(), name='tutor_course_list'),
+    path('tutor_course_detail/<int:course_id>', TutorCourseDetail.as_view(), name='tutor_course_detail'),
+    path('tutor_lecture_detail/<int:lecture_id>', TutorLectureDetail.as_view(), name='tutor_lecture_detail'),
 ]
--- tutors/views.py
+++ tutors/views.py
@@ -1,8 +1,9 @@
 from typing import Any
+from django.db import models
 from django.db.models.query import QuerySet
 from django.shortcuts import render
 from courses.models import Course, Lecture
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 
@@ -12,4 +13,28 @@ class TutorCourseList(ListView):
     ordering = ['term', 'course_number']
     
     def get_queryset(self):
+        #print(self.request.__dict__)
+        #print(self.request.GET)
         return Course.objects.filter(tutor=self.request.user)
+    
+
+class TutorCourseDetail(DetailView):
+    model = Course
+    pk_url_kwarg = 'course_id'
+    template_name = 'tutors/tutor_course_detail.html'
+
+    def get_queryset(self):
+        return Course.objects.filter(pk=self.kwargs['course_id'], tutor=self.request.user)
+        
+
+class TutorLectureDetail(DetailView):
+    model = Lecture
+    pk_url_kwarg = 'lecture_id'
+    template_name = 'tutors/tutor_lecture_detail.html'
+
+    def get_queryset(self):
+        lecture = Lecture.objects.get(pk=self.kwargs['lecture_id'])
+        if lecture.course.tutor == self.request.user:
+            return Lecture.objects.filter(pk=self.kwargs['lecture_id'])
+        else:
+            return Lecture.objects.none()
