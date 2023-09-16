a3253ca5ee3bdeb06c3ea84404d7dd4d30449019
liuxinyu1984 <liuxinyu1984@users.noreply.github.com>
2023-09-15 21:37:11 -0700

    connect to postgresql on railway

--- backend/settings.py
+++ backend/settings.py
@@ -90,13 +90,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if not DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'HOST': os.environ.get('PGHOST'),
+            'NAME': os.environ.get('PGDATABASE'),
+            'PASSWORD': os.environ.get('PGPASSWORD'),
+            'PORT': os.environ.get('PGPORT'),
+            'USER': os.environ.get('PGUSER')
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
